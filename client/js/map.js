60943533da2bd1ca29d37f85087d3a6c55239c05
Leonel Castañeda <leodcastaneda@gmail.com>
2018-12-07 00:23:00 -0600

    url functionality

--- client/js/map.js
+++ client/js/map.js
@@ -1,12 +1,37 @@
 // init map
 function buildMap(){
-  var map = L.map("map", { zoomControl:false }).setView([24.59, -103.14], 5);
+  var map = L.map("map", {
+    zoomControl:false,
+    // crs: L.CRS.EPSG4326
+  }).setView([24.59, -103.14], 5);
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  var hash = new L.Hash(map);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
+  buildButtons(map);
 
+  // addLayerControl
+  var layerControl = L.control.layers({}, {}, {position: "topleft", collapsed: false}).addTo(map);
+
+  map.addWmsLayer = function(geoserverId, title){
+    var wmsLayer = L.tileLayer.wms("https://geo.datos.gob.mx/geoserver/ows?", {
+      layers: "ckan:" + geoserverId,
+      transparent: true,
+      format: "image/png",
+      // crs: L.CRS.EPSG4326
+    }).addTo(map);
+
+    layerControl.addOverlay(wmsLayer, title)
+  }
+
+  window.map = map;
+}
+
+function buildButtons(map){
+  // locate control
   var lc = L.control.locate({
     position: "topright",
     icon: "fa fa-location-arrow",
@@ -17,115 +42,34 @@ function buildMap(){
       var link = L.DomUtil.create('a', 'leaflet-bar-part leaflet-bar-part-single', container);
       link.title = options.strings.title;
 
-      console.log(options);
       var icon = L.DomUtil.create(options.iconElementTag, options.icon + " white-icon", link);
 
       return { link: link, icon: icon };
     }
   }).addTo(map);
 
-  addButtonToMap(map, "fa-search-plus", function(evt){
+  // zoom +
+  addButtonToMap(map, "fa-search-plus", "topright", function(evt){
     map.zoomIn();
   });
 
-  addButtonToMap(map, "fa-search-minus", function(){
+  // zoom -
+  addButtonToMap(map, "fa-search-minus", "topright", function(){
     map.zoomOut();
   });
 
-  window.map = map;
+
+  // addLayer
+  addButtonToMap(map, "fa-plus", "topleft", function(evt){
+    $('#addLayerModal').modal({})
+  });
 }
 
 
-function addButtonToMap(map, icon, callback){
+function addButtonToMap(map, icon, position, callback){
   new L.Control.SimpleButton({
+    position: position,
     click: callback,
     faIcon: "fa " + icon
   }).addTo(map);
-}
-
-
-function addButton(map){
-  L.Control.RemoveAll = L.Control.extend({
-    options: {
-      position: 'topleft',
-    },
-    onAdd: function (map) {
-      var controlDiv = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
-
-      L.DomEvent
-      .addListener(controlDiv, 'click', L.DomEvent.stopPropagation)
-      .addListener(controlDiv, 'click', L.DomEvent.preventDefault)
-      .addListener(controlDiv, 'click', function () {
-        drawnItems.clearLayers();
-      });
-
-      var controlUI = L.DomUtil.create('a', '', controlDiv);
-      controlUI.title = 'Remove All Polygons';
-      controlUI.href = '#';
-
-      return controlDiv;
-    }
-  });
-
-  var removeAllControl = new L.Control.RemoveAll();
-  map.addControl(removeAllControl);
-}
-
-
-
-
-var AddLayerControl = function(opt_options) {
-  var options = opt_options || {};
-  this.active = options.active || false;
-
-  var this_ = this;
-
-  var handleAddLayer = function(e) {
-    $("#newLayerDialog").modal("show");
-  };
-
-  // this will be the ui of the component
-  var anchor = document.createElement('a');
-  anchor.href = '#add-layer-button';
-  anchor.innerHTML = 'Agregar capa';
-
-
-  // bind to click and touchevents to support mobile
-  anchor.addEventListener('click', handleAddLayer, false);
-  anchor.addEventListener('touchstart', handleAddLayer, false);
-
-  var element = document.createElement('div');
-  element.className = 'ol-control draw-point ol-unselectable';
-  element.appendChild(anchor);
-
-  ol.control.Control.call(this, {
-    element: element,
-    target: options.target
-  });
-};
-
-
-function buildPopup(map, wmsLayer, mapView){
-  // Popup
-  var infoPopup = new ol.Overlay.Popup();
-  map.addOverlay(infoPopup);
-
-  map.on("singleclick", function(evt) {
-    var viewResolution = /** @type {number} */ (mapView.getResolution());
-    var coordinates = evt.coordinate;
-
-    var url = wmsLayer.getGetFeatureInfoUrl(coordinates, viewResolution, "EPSG:4326", {"info_format": "application/json"});
-
-    $.get(url, function(data){
-      var featureInfo = data.features[0].properties;
-
-      var html = "<div>";
-      for(var key in featureInfo){
-        html += "<small><b>" + key + "</b>: " + featureInfo[key] + "</small><br>";
-      }
-      html += "</div>";
-
-      infoPopup.show(coordinates, html);
-    });
-  });
 }
--- client/js/master.js
+++ client/js/master.js
@@ -1,45 +1,111 @@
-function searchLayerHandler(){
-  console.log("Searching layer")
-  // text = $("#search-layer-inpupt").val()
+function apiSearch(){
+  text = $("#inputSearch").val();
 
-  var api_geo_response = [
-    {
-      geo_id: "d5618157_bbeb_473c_b4aa_a9c641da3a79",
-      geo_title: "Chile verde año agricola 2013",
-      abstract: ""
-    },
-    {
-      geo_id: "49c7971b_f932_4c36_8955_cda297d13443__localidad50_a",
-      geo_title: "Conjunto de datos vectoriales de información topográfica G14A25 (Don Martín) escala 1:50 000 serie III - localidad50_a",
-      abstract: "Contienen información sobre los diversos datos espaciales presentes en los conjuntos de datos, como curvas de nivel, hidrografía, vías de comunicación, localidades, entre otros. Estos rasgos son representados digitalmente por un componente geométrico (puntos, líneas o áreas) y componentes descriptivos (los atributos del dato). Los topónimos o nombres geográficos, se incluyen como atributos propios de cada uno de los datos que conforman cada conjunto de datos y que por su naturaleza los requieren. En esta serie se actualizaron las localidades, vías de comunicación y cuerpos de agua, por ser estos los rasgos con mayor dinámica con respecto al tiempo, además de todos aquellos objetos que por su relación espacial con las anteriores necesiten ajustarse.",
-      keywords: "inegi, acueducto"
-    },
-    {
-      geo_id: "a8694801_716f_4487_8c4e_e1f099c28888",
-      geo_title: "Avena graño año agricola 2013",
-      abstract: ""
-    },
-    {
-      geo_id: "37f29fcc_4b57_4962_a52c_9cebefae615d",
-      geo_title: "Comercio al por mayor 01/2016",
-      abstract: "Ofrece datos de identificación, ubicación, actividad económica y tamaño de los negocios activos en el territorio nacional, actualizados, fundamentalmente, en el segmento de los establecimientos grandes. Esta versión se actualizó con base en los resultados definitivos de los Censos Económicos 2014 y del operativo de verificación de unidades económicas nuevas realizado por el INEGI en el segundo semestre del 2015. Resultado de esto, en el DENUE 01-2016 se publican los datos de 5 millones 4 mil 986 negocios a nivel nacional. De ellos, 4 millones 912 mil 324 ya estaban registrados en la versión anterior del DENUE y se incluyen 92 mil 662 establecimientos nuevos. Este archivo contiene 35,368 registros que corresponde al total de unidades económicas activas en el estado de Colima.",
-      keywords: "inegi, agricultura,alimentos"
-    }
-  ]
+  console.log("Searching layer " + text);
 
-  api_geo_response.forEach(function(layer){
-    var html = "<a onclick='map.addWmsLayer(\"" + layer.geo_id + "\", \"" + layer.geo_title + "\")'>";
+  var insertRow = true;
+  var closeRow = false;
 
-    html += "<b>" + layer.geo_title + "</b><br>"
-    html += "<small>" + layer.abstract + "</small>"
+  $.getJSON("https://api.datos.gob.mx/v2/ckan-geoserver?pageSize=20&page=100", function(data){
+    data["results"].forEach(function(element){
+      var html = "";
+      if( insertRow ){
+        html += "<div class='row'>"
+        insertRow = false;
+      }else{
+        closeRow = true;
+      }
 
-    html += "</a>";
+      // tag
+      html += "<div class='col-sm-6 result-element' onclick=\"addLayerToMap('{0}', '{1}');\"><div class='col-sm-1'><span class='tag-icon tag-geoespacial'></span></div>".format(element.geoserver, element.name_resource);
 
-    $("#layer_result_list").append(html);
-  })
+      // resource info
+      html += "<div class='col-sm-9'><p class='title'>{0}</p><small>{1}</small></div>".format(element.name_resource, element.description);
+
+      // organization
+      html += "<div class='col-sm-2'><div class='resource-item-org'><strong><a href='{1}'>{0}</a></strong></div></div></div>".format(element.organization.title, "url");
+
+      if( closeRow ){
+        html += "</div>"
+        insertRow = true;
+        closeRow = false;
+
+        $("#searchResult").append(html);
+      }
+
+
+
+      // addLayerToMap(element.geoserver, element.name_resource);
+    });
+  });
 }
+
+function addLayerToMap(geoserverId, title){
+  map.addWmsLayer(geoserverId, title);
+
+  $('#addLayerModal').modal("hide");
+}
+
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+  sURLVariables = sPageURL.split('&'),
+  sParameterName,
+  i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    }
+  }
+};
 
 
 $(function(){
+  if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+        ;
+      });
+    };
+  }
+
+  if (getUrlParameter("embeded") == "true"){
+    $("#header").hide();
+    $("#footer").hide();
+
+    $("body").css("padding", "0");
+    $("body").css("margin", "0");
+
+    $("#map").height( $("html").height() );
+  } else {
+    $("#map").height( $("html").height() - $("#header").position().top - $("#header").height() - 100 );
+  }
+
+
+  if (getUrlParameter("config")){
+    var config = getUrlParameter("config").slice("1", "-1");
+
+    for (geoserverId of config.split(",")){
+
+      $.getJSON("https://api.datos.gob.mx/v2/ckan-geoserver?geoserver=" + geoserverId, function(data){
+        data["results"].forEach(function(element){
+          addLayerToMap(element.geoserver, element.name_resource);
+        });
+      });
+
+    }
+  }
+
+  $('#addLayerModal').on('shown.bs.modal', function (e) {
+    $("#searchResult").empty();
+    apiSearch();
+  });
+
   buildMap();
 });
